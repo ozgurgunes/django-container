c246251c5d085f67d70cc1390a065772db869789
Ozgur Gunes <o.gunes@gmail.com>
2012-02-13 19:13:17 +0200

    cleaned apps

--- apps/registration/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-from apps.registration.tests.backends import *
-from apps.registration.tests.forms import *
-from apps.registration.tests.models import *
-from apps.registration.tests.views import *
